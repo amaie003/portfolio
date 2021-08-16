4139ccb531a00b0b6470e34cc5a696cb4851f225
Efan <amaie003@ucr.edu>
2021-08-16 00:16:09 -0700

    update

--- src/Main.js
+++ src/Main.js
@@ -1,14 +1,13 @@
 import { react, Component } from "react";
 import "./styles/Main.scss";
-import bgImg from './img/mainBG.png';
-import { FaBeer } from 'react-icons/fa';
+import bgImg from "./img/mainBG.png";
+import { FaChevronDown } from "react-icons/fa";
+import { IconContext } from "react-icons";
 
 class Main extends Component {
   constructor() {
     super();
-   
   }
-  
 
   getsubTitle = () => {
     return (
@@ -28,9 +27,36 @@ class Main extends Component {
   render() {
     return (
       <div className="main-container">
-
-        <div className="main-main" > 
-        <img className="main-bg" src={bgImg} alt="Aierfan Portfoil Main Background"/>
+        <div className="main-main">
+          <img
+            className="main-bg"
+            src={bgImg}
+            alt="Aierfan Portfoil Main Background"
+          />
+          <div className="main-scroll_indicater">
+          <IconContext.Provider
+            value={{ color: "white", size:20,className:"main-down_arrow main-down_arrow1"}}
+          >
+            <div>
+              <FaChevronDown/>
+            </div>
+          </IconContext.Provider>
+          <IconContext.Provider
+            value={{ color: "white", size:20,className:"main-down_arrow main-down_arrow2"}}
+          >
+            <div>
+              <FaChevronDown/>
+            </div>
+          </IconContext.Provider>
+          <IconContext.Provider
+            value={{ color: "white", size:20,className:"main-down_arrow main-down_arrow3"}}
+          >
+            <div>
+              <FaChevronDown/>
+            </div>
+          </IconContext.Provider>
+          <div className="main_scroll_text">Scroll Down</div>
+            </div>
           <div className="main-bg_gradient"></div>
           <div className="main-text_zone">
             <h1 className="main-main_title">{this.props.mainTitle} </h1>
@@ -38,12 +64,18 @@ class Main extends Component {
             <h1 className="main-main_title">{this.props.mainTitleSub}</h1>
             <br />
             <h2 className="main-sub_title">{this.getsubTitle()}</h2>
-            <br/>
-            <button className="main-contact_button blue_button">Email Me</button>
+            <br />
+            <button className="main-contact_button blue_button">
+              Email Me
+            </button>
           </div>
         </div>
         <a id="about"></a>
-        <div>about aboutaboutaboutabout</div>
+        <div>about aboutaboutaboutabout
+        <div class="chevron"></div>
+          <div class="chevron"></div>
+          <div class="chevron"></div>
+        </div>
       </div>
     );
   }
--- src/data/data.js
+++ src/data/data.js
@@ -1,6 +1,6 @@
 export const portfolioData = 
     {
-        "welcome":{"main_title":"Hi, I Am Aierfan, ","main_title_sub":"Full-Stack Developer","sub_title":["React App", "MySQL", "REST API Specialist"]},
+        "welcome":{"main_title":"Hi, I Am Aierfan, ","main_title_sub":"Full-Stack Developer","sub_title":["React", "MySQL", "REST API Specialist","Amazon Web Services"]},
         
         "about":[],
         "work_experiences":[],
